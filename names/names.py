b2bdc0a8513d63aa4fdea5986ec4e387a4ffc25a
= <=>
2020-05-29 20:41:50 -0700

    finished the names

--- names/names.py
+++ names/names.py
@@ -13,10 +13,26 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# sets only allow unique items.. 
+# so technically it can be used as a filter
+seen = set()
+names = names_1 + names_2
+
+for name in names:
+    # if the name is not in the set
+    if name not in seen:
+        seen.add(name)
+    else:
+        # its a duplicate
+        duplicates.append(name)
+        
+
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
